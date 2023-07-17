342425ae37e8515d7d50b338f550a5fed2911f48
CEgerer93 <cegerer@protonmail.com>
2023-07-17 17:28:08 -0400

    Removed feature/gritas-proc pieces

--- src/Applications/GrITAS_Proc/env_gritas.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-#
-#module load anaconda/py3.9
-
-source $NOBACKUP/GrITAS/install/bin/g5_modules.sh
-export PATH=$PATH:/discover/nobackup/cegerer/GrITAS/install/bin
-
-# Better way to set these?
-export PREFGRITAS=/discover/nobackup/cegerer/GrITAS/src
